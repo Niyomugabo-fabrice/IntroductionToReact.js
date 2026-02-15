9b9db83708bb86fd1dd0187df13608a5b609c4a0
Niyomugabo-fabrice <nfabrice058@gmail.com>
2026-02-15 15:49:58 +0200

    adding the headers ant the css

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from "./Components/Home.tsx"
 import Product from "./Components/Products.tsx"
+import Header from "./Components/Header.tsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <p>
+    <Header/>
     <Home/>
     <Product/>
     </p>
---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import "./Headers.css"
+export default function Header()
+{
+    return(
+        <div className="links">
+            <a href="#">Home</a>
+            <a href="#">Product</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+        </div>
+    )
+}
