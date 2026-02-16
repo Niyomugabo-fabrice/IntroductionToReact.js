00441e69d7d125142a30bfd8ef528b0f1fcd882b
Niyomugabo-fabrice <nfabrice058@gmail.com>
2026-02-16 16:42:20 +0200

    removing unneccessary stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Home from "./Components/Home.tsx"
-import Product from "./Components/Products.tsx"
 import Header from "./Components/Header.tsx"
 import AppRoute from "./AppRoutes/AppRoutes.tsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <p>
     <AppRoute/>
